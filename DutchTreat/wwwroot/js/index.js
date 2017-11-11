e186d5f96c9cab842571db86853288495dcd4245
Kiran Shahi <kiran.shahi.np@gmail.com>
2017-11-11 12:02:28 +0545

    jquery and bootstap has been added.

--- DutchTreat/wwwroot/js/index.js
+++ DutchTreat/wwwroot/js/index.js
@@ -1,1 +1,23 @@
-﻿
+﻿$(document).ready(function () {
+    var theForm = $("#theForm");
+    theForm.hide();
+
+    var button = $("#buyButton");
+    button.on("click", function () {
+        alert("Buying Item");
+    });
+
+    var productInfo = $(".product-props li");
+    productInfo.on("click", function () {
+        console.log("You clicked on " + $(this).text());
+    });
+
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.fadeToggle(1000);
+    });
+
+});
