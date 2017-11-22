6314c13fac9a7af5e2b4119106995c8a51cde364
Kiran Shahi <kiran.shahi.np@gmail.com>
2017-11-22 21:28:32 +0545

    master layout added.

--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -15,13 +15,25 @@ namespace DutchTreat
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseDefaultFiles();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            //app.UseDefaultFiles();
             app.UseStaticFiles();
+
+            app.UseMvc(cfg =>
+            {
+                cfg.MapRoute("Default",
+                    "{controller}/{action}/{id?}",
+                    new { controller = "App", Action = "Index" });
+            });
         }
     }
 }
